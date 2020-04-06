c544e35439cbeac5937502b1ae3ff17c2616194b
Jimena_Gensollen <jimenagensollen@gmail.com>
2020-04-06 19:10:35 -0400

    completed

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// TODO
+class GroceryList extends React.Component {
+   constructor(props){
+       super(props);
+
+       this.state = {
+           bold: false
+       };
+   }
+
+   onMouseOver() {
+       this.setState({
+           bold: !this.state.bold
+       });
+   }
+
+   render(){
+       let style = {
+           fontWeight: this.state.bold ? 'bolder' : 'normal'
+           };
+       
+       return (
+        
+            <li style={style} onMouseOver={this.onMouseOver.bind(this)}> {this.props.item} </li>
+        );
+   }
+}
+
+
+var List = (props) =>(
+    <ul>
+        {props.items.map(item =>
+            < GroceryList key = {item} item = {item} /> )}
+    </ul>
+);
+
+
+ReactDOM.render(<List items = {['Rice', 'Bread']} />, document.getElementById("app"));
